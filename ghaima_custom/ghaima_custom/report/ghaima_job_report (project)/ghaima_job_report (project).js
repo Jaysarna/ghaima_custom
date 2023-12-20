276b65a979f5e3be673ee1276b3617f15ff4151b
frappe <frappe@Gigabyte-GD-G5>
2023-12-20 15:17:01 +0530

    fix

---
+++ ghaima_custom/ghaima_custom/report/ghaima_job_report (project)/ghaima_job_report (project).js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, jay and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["GHAIMA Job report (project)"] = {
+	"filters": [
+
+	]
+};
